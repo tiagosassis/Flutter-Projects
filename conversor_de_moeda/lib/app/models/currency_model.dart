1c924d1977e7369fa39e0bc84da80ebeb92afa04
Tiago Silva de Assis <tiagoassisdev@gmail.com>
2023-02-13 15:42:17 -0300

    .

--- conversor_de_moeda/lib/app/models/currency_model.dart
+++ conversor_de_moeda/lib/app/models/currency_model.dart
@@ -13,7 +13,7 @@ class CurrencyModel{
             CurrencyModel(
                 name: 'Real', real: 1.0, dollar: 0.18, euro: 0.15, bitcoin: 0.000016), 
             CurrencyModel(
-                name: 'Dollar', real: 5.65, dollar: 1.0, euro: 0.85, bitcoin: 0.000088), 
+                name: 'Dollar', real: 5.65, dollar: 1.0, euro: 0.85, bitcoin: 0.000088),
             CurrencyModel(
                 name: 'Euro', real: 6.62, dollar: 1.17, euro: 1.0, bitcoin: 0.000010), 
             CurrencyModel(
